8839cdd82596bac932a4c1293a8479aed3814d31
LoneDev <gabquelyn@gmail.com>
2023-08-11 23:46:37 +0100

    working of secured route

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -10,7 +10,7 @@ interface loginReq {
   password: string;
 }
 export const loginController = expressAsyncHandler(
-  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  async (req: Request, res: Response): Promise<any> => {
     const { email, password }: loginReq = req.body;
     if (!email || !password)
       return res.status(400).json({ message: "All fields are required" });
@@ -23,7 +23,7 @@ export const loginController = expressAsyncHandler(
     const accessToken = jwt.sign(
       {
         UserInfo: {
-          username: foundUser.email,
+          eamil: foundUser.email,
           roles: foundUser.roles,
         },
       },
@@ -51,12 +51,47 @@ export const loginController = expressAsyncHandler(
 );
 
 export const refreshController = expressAsyncHandler(
-  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  async (req: Request, res: Response): Promise<any> => {
     const cookies = req.cookies;
-    if (!cookies?.jwt) return res.status(401).json({ message: "Unauthorized" });
+    if (!cookies?.jwt) return res.status(403).json({ message: "Unauthorized" });
     const refreshToken = cookies.jwt;
     // verify the refresh token
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET as string,
+      async (error: any, decoded: any) => {
+        if (error) return res.status(403).json({ message: "Forbidden" });
+        const foundUser = await User.findOne({ email: decoded?.email }).exec();
+        if (!foundUser)
+          return res.status(400).json({ message: "Unauthorized" });
+        // create the access token
+        const accessToken = jwt.sign(
+          {
+            UserInfo: {
+              email : foundUser.email,
+              roles: foundUser.roles,
+            },
+          },
+          process.env.ACCESS_TOKEN_SECRET as string,
+          { expiresIn: "30s" }
+        );
+        res.json(accessToken);
+      }
+    );
   }
 );
 
-export const logoutController = async () => {};
+export const logoutController = async (
+  req: Request,
+  res: Response
+) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204) //no content;
+  res.clearCookie("jwt", {
+    httpOnly: true, //only accessible through a web browser
+    secure: true, //https
+    sameSite: "none", // cross site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, //
+  });
+  res.json({ message: "Cookie cleared" });
+};
---
+++ src/middlewares/verifyJWT.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+interface CustomRequest extends Request {
+    email?: string;
+    roles?: string[];
+  }
+
+export default async function verifyJWT(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader: string | string[] | undefined =
+    req.headers?.authorization || req.headers?.Authorization;
+  const authValue = authHeader as string;
+  if (!authValue?.startsWith("Bearer "))
+    return res.status(401).json({ message: "Unauthorized" });
+
+  const token = authValue.split(" ")[0];
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET as string,
+    (error: any, decoded: any) => {
+      if (error) return res.status(403).json({ message: "Forbidden" });
+      (req as CustomRequest).email = decoded.UserInfo.email;
+      (req as CustomRequest).roles = decoded.UserInfo.roles;
+    }
+  );
+}
