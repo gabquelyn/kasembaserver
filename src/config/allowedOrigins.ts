ec01c1535ddaf921754df3e2b28bbac0567f3a87
LoneDev <gabquelyn@gmail.com>
2023-11-03 21:25:22 +0000

    added production site to cors

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,4 +1,7 @@
 const allowedOrigins = [
-    "http://localhost:3000"
-]
+  "http://localhost:3000",
+  "https://www.karsemba.com",
+  "https://karsemba.com",
+  "http://karsemba.com",
+];
 export default allowedOrigins;
